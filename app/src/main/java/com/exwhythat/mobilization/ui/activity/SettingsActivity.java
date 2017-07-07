283eb72088687397607df70e063ef0e16635005e
illarionovR <illroman@yandex.ru>
2017-07-07 21:50:07 +0300

    Заполнил NavigationDrawer. Добавил активити "Настройки" и "О программе".

---
+++ app/src/main/java/com/exwhythat/mobilization/ui/activity/SettingsActivity.java
@@ -0,0 +1,42 @@
+package com.exwhythat.mobilization.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.exwhythat.mobilization.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        initActivity();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void initActivity() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, SettingsActivity.class));
+    }
+}
