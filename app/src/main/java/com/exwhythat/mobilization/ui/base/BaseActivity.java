9b80028e821c6aa4b0b8f232b232925b4f798cda
illarionovR <illroman@yandex.ru>
2017-07-10 18:02:38 +0300

    Задействовал ButterKnife

--- app/src/main/java/com/exwhythat/mobilization/ui/base/BaseActivity.java
+++ app/src/main/java/com/exwhythat/mobilization/ui/base/BaseActivity.java
@@ -8,6 +8,8 @@ import com.exwhythat.mobilization.di.component.ActivityComponent;
 import com.exwhythat.mobilization.di.component.DaggerActivityComponent;
 import com.exwhythat.mobilization.di.module.ActivityModule;
 
+import butterknife.Unbinder;
+
 /**
  * Created by exwhythat on 09.07.17.
  */
@@ -15,6 +17,8 @@ import com.exwhythat.mobilization.di.module.ActivityModule;
 public class BaseActivity extends AppCompatActivity implements BaseView {
 
     private ActivityComponent mActivityComponent;
+
+    private Unbinder mUnbinder;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,7 +28,19 @@ public class BaseActivity extends AppCompatActivity implements BaseView {
                 .build();
     }
 
+    public void setUnbinder(Unbinder unbinder) {
+        mUnbinder = unbinder;
+    }
+
     public ActivityComponent getActivityComponent() {
         return mActivityComponent;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mUnbinder != null) {
+            mUnbinder.unbind();
+        }
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/exwhythat/mobilization/ui/main/MainActivity.java
+++ app/src/main/java/com/exwhythat/mobilization/ui/main/MainActivity.java
@@ -17,11 +17,23 @@ import com.exwhythat.mobilization.ui.settings.SettingsActivity;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity
         implements MainView, NavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     MainPresenter<MainView> mPresenter;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,23 +42,21 @@ public class MainActivity extends BaseActivity
     }
 
     private void initActivity() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        initNavigationDrawer(toolbar);
         getActivityComponent().inject(this);
+        setUnbinder(ButterKnife.bind(this));
+        setSupportActionBar(mToolbar);
+        initNavigationDrawer(mToolbar);
         mPresenter.onAttach(this);
         mPresenter.onSomeActionFromActivity();
     }
 
     private void initNavigationDrawer(Toolbar toolbar) {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNavigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,16 +92,14 @@ public class MainActivity extends BaseActivity
                 throw new IllegalStateException("Navigation drawer undeclared item");
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
